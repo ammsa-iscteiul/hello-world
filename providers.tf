771402d7b8ee55ac9bd9fcdbe870507dc45441b0
Alexandre Silva <alexmmsilva@hotmail.com>
2024-04-22 13:03:32 +0100

    Add files via upload

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  tenant_id       = "726174dc-30ab-40e1-85fc-60cda0e0bd81"
+}
