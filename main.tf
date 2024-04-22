6277e6c1c310c6c1e6a31daa23892fe193ac02db
Alexandre Silva <alexmmsilva@hotmail.com>
2024-04-22 17:17:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
-}
-
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
