9aaa1b853c56d76bb53aefffa2fe1ea529b0c7ba
Alexandre Silva <alexmmsilva@hotmail.com>
2024-04-22 13:07:19 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  tenant_id       = "726174dc-30ab-40e1-85fc-60cda0e0bd81"
+}
+
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
